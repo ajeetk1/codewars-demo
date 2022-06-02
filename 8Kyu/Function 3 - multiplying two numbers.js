21303f590df2a0df0143ee1ddd42e2d4af15e274
AjeetK <ajeet.khamesra1@outlook.com>
2022-06-02 12:39:21 +0530

    codewars -8kyu

---
+++ 8Kyu/Function 3 - multiplying two numbers.js
@@ -0,0 +1,19 @@
+//Description  ✅
+
+//Implement a function which multiplies two numbers
+
+// My Solution
+
+function multiply(a,b){
+    return a*b
+  }
+  let result=multiply(3,4);
+  console.log(result)
+
+  //let multiply = (a, b) => a * b;
+
+  //
+  function multiply(a, b){
+  if(typeof a == 'number' && typeof b == 'number')
+  return a * b;  
+}
