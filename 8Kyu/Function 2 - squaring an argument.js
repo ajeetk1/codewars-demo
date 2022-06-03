dfe1ac3b80a7373b41bdec0a88b05738f5c26e58
AjeetK <ajeet.khamesra1@outlook.com>
2022-06-04 01:42:47 +0530

    My codewar -First Try Solution 8kyu

---
+++ 8Kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,9 @@
+//Description ✅
+//Now you have to write a function that takes an argument and returns the square of it.
+
+//Solution
+// Write the "square"-function here
+function square (x){
+    return x*x;
+    
+  }
