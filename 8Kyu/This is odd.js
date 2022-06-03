2c0faad46eb86a365482b181ba6afa92f000d4e3
AjeetK <ajeet.khamesra1@outlook.com>
2022-06-04 02:35:59 +0530

    My Codewar more solution

---
+++ 8Kyu/This is odd.js
@@ -0,0 +1,33 @@
+//This is odd
+
+//Create a function that checks if a number is odd.
+
+//Expect negative and decimal numbers too. Remember... all negative numbers can also be either odd or even.
+
+//Decimal numbers always return false
+//Examples
+
+//Example:
+//is_odd(2)--> false
+//is_odd(5)--> true
+//is_odd(4)--> false
+//is_odd(-17)--> true
+//is_odd(-7.0)--> true
+//is_odd(-7.1)--> false
+//is_odd(4.23)--> false
+//is_odd(5.0)--> true
+//is_odd(5.23)--> false
+
+//My Solution:
+
+function  is_odd(n) {
+  return n%2==1;
+end
+}
+//
+function is_odd(n){
+  n % 2 == 1 ? true : false
+end
+}
+
+
